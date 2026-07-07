d95bc39c3ca5877cc870037e7a985880a3c3c2ea
Oriol599 <arge_prog@proton.me>
2026-07-07 13:41:32 +0200

    config: añadir pipeline de CI/CD y adaptar estructura para Vite

---
+++ src/app.test.js
@@ -0,0 +1,5 @@
+import { expect, test } from 'vitest';
+
+test('Verificación inicial del pipeline', () => {
+  expect(true).toBe(true);
+});
---
+++ vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: './', // Esto asegura que las rutas en gh-pages sean relativas
+});
